80c77523f9a5b8275d2212322152b45616da5c4f
tolgakasalk@outlook.com <tolgakasalk@outlook.com>
2021-02-16 16:47:52 +0300

    16.02.2021

---
+++ WebAPI/Controllers/CustomersController.cs
@@ -0,0 +1,70 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        ICustomersService _customersService;
+
+        public CustomersController(ICustomersService customersService)
+        {
+            _customersService = customersService;
+        }
+
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _customersService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Customers customers)
+        {
+            var result = _customersService.Add(customers);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("delete")]
+        public IActionResult Delete(Customers customers)
+        {
+            var result = _customersService.Delete(customers);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(Customers customers)
+        {
+            var result = _customersService.Update(customers);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+    }
+}
